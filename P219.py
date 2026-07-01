1792177ed43225f04a3892cbcee4835b8c3b2feb
Yug Katyal <yugkatyalgtrvocal@gmail.com>
2026-07-01 21:52:58 +0530

    Day 1

---
+++ P219.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        slide = set()
+
+        for i in range(len(nums)):
+
+            if nums[i] in slide:
+                return True
+
+            slide.add(nums[i])
+
+            if len(slide) > k:
+                slide.remove(nums[i - k])
+
+        return False
