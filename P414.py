dc4ee96dca287867ce34f2670cefddac87075692
Yug Katyal <yugkatyalgtrvocal@gmail.com>
2026-07-02 23:25:58 +0530

    Third Max Number

---
+++ P414.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def thirdMax(self, nums):
+        unique = sorted(set(nums))
+
+        if len(unique) >= 3:
+            return unique[-3]
+
+        return unique[-1]
