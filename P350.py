674c2ace0c3c98326eda209fc6a800882d7c0e62
Yug Katyal <yugkatyalgtrvocal@gmail.com>
2026-07-02 23:14:58 +0530

    Intersection of 2 arrays - II

---
+++ P350.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        # ans = []
+        # for i in range(len(nums2)):
+        #     if nums2[i] in nums1:
+        #         ans.append(nums2[i])
+        #         nums1.remove(nums2[i])
+
+        # return ans
+
+        #USING HASH MAP
+        
+        freq = {}
+
+        # Build frequency map
+        for num in nums1:
+            freq[num] = freq.get(num, 0) + 1
+
+        # Find intersection
+        ans = []
+        for num in nums2:
+            if freq.get(num, 0) > 0:
+                ans.append(num)
+                freq[num] -= 1
+
+        return ans
+
+        
+
+        
