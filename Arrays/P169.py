19b33b2be98f6b128f8389f2cba8e45b8ecfc74e
Yug Katyal <yugkatyalgtrvocal@gmail.com>
2026-07-04 18:44:37 +0530

    Majority Element

--- Arrays/P169.py
+++ Arrays/P169.py
@@ -4,6 +4,7 @@ class Solution(object):
         for num in nums:
             freq[num] = freq.get(num,0)+1
             if freq[num] > len(nums)//2:
+                
                 return num
 
         
