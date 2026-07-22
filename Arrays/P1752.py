d63824ebb192cbd79a2bedf0f5315026fe906926
Yug Katyal <yugkatyal.ai@gmail.com>
2026-07-22 20:00:06 +0530

    26 and 1752

---
+++ Arrays/P1752.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def check(self, nums):
+        n = len(nums)
+        if n==1:
+            return True
+        count = 0
+        for i in range(n):
+            if nums[i]>nums[(i+1)%n]:
+                count+=1
+        return count<=1
