7dad4a75469a7a169df935f99462dac7d71d13b2
Yug Katyal <yugkatyal.ai@gmail.com>
2026-07-23 23:18:55 +0530

    latest

---
+++ Strings/P1021.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def removeOuterParentheses(self, s):
+        result = []
+        bal = 0
+        start = 0
+
+        for i,char in enumerate(s):
+            if char =='(':
+                if bal ==0:
+                    start = i
+                bal +=1
+            elif char == ')':
+                bal -=1
+                if bal ==0:
+                    result.append(s[start+1:i])
+
+        return "".join(result)
+        
